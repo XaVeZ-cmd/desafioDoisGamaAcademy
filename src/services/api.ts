5306f9c219182452b519002393a58b14334033fa
XaVeZ-cmd <thiago.amorim_2@live.com>
2021-07-24 17:27:56 -0300

    Teste api 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,32 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://raw.githubusercontent.com/XaVeZ-cmd/desafioDoisGamaAcademy/master/server.json'
-})
+function api(request: { JSON: any; }, response: any){
+  response.json(
+    {
+      "produtos": [
+        {
+          "id": 1,
+          "photo": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWYK2?wid=2000&hei=2000&fmt=jpeg",
+          "name": "IPhone",
+          "description": "Descrição do produto",
+          "price": 8000
+        },
+        {
+          "id": 2,
+          "photo": "https://www.casasbahia-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1620261745",
+          "name": "Galaxy",
+          "description": "Descrição do produto",
+          "price": 3000
+        },
+        {
+          "id": 3,
+          "photo": "https://cdn.awsli.com.br/600x700/1417/1417526/produto/100422729ecd3963191.jpg",
+          "name": "Pocophone",
+          "description": "Descrição do produto",
+          "price": 2500
+        }
+        
+      ]
+    }
+  )
+}
 
 export default api;
